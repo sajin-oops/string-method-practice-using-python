d75448d372f21b3bf6513a2d3cc524fc29ad7516
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-11 20:48:59 +0530

    Add files via upload

---
+++ string assignment.py
@@ -0,0 +1,31 @@
+str = "happy"
+print(str[0])
+print(str[0:2])
+print(str[0:3])
+print(str[0:4])
+print(str[0:5])
+
+# output
+'''
+h
+ha
+hap
+happ
+happy
+
+'''
+word = "sajin"
+print(word[-1::])
+print(word[-2::])
+print(word[-3::])
+print(word[-4::])
+print(word[-5::])
+# output
+
+'''
+n
+in
+jin
+ajin
+sajin
+'''
